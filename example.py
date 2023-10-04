503c192dc1784e70f088db56a6082b9b27ab3bff
cecilevdstappen@hotmail.com <cecile.vanderstappen@tno.nl>
2023-10-04 15:30:58 +0200

    added test for add function

---
+++ example.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a + b  # do not change this line until prompted to do so.
+
+def test_add():
+	assert add(4,5)==  9
+
+#def test_substract():
+#	assert substract(14,3) ==11
